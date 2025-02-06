8476b942fe51a7b6df5f458196380d40f45bbec1
shirodta <107359778+shirodta@users.noreply.github.com>
2025-02-06 02:38:20 -0500

    Create custom-script2.js

---
+++ custom-script2.js
@@ -0,0 +1,5 @@
+setTimeout(() => {
+var script = document.createElement("script");
+script.src = "//pl24962765.profitablecpmrate.com/fb/13/3c/fb133ca34aa7beeecb5577b3814b9e77.js";
+document.body.appendChild(script);
+}, 3000); // Se carga después de 3 segundos
